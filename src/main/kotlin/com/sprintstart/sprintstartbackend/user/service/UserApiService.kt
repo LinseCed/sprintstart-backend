385a49e600619a68237dcadc86f4cabf39e55dc2
BrainSkript <a.johnsen02@gmail.com>
2026-05-25 18:13:06 +0200

    Added UserApi.kt with external methods for other modules

---
+++ src/main/kotlin/com/sprintstart/sprintstartbackend/user/service/UserApiService.kt
@@ -0,0 +1,14 @@
+package com.sprintstart.sprintstartbackend.user.service
+
+import com.sprintstart.sprintstartbackend.user.external.UserApi
+import com.sprintstart.sprintstartbackend.user.repository.UserRepository
+import java.util.UUID
+
+class UserApiService (
+    private val userRepository: UserRepository
+): UserApi {
+
+    override fun exists(id: UUID): Boolean {
+        return userRepository.existsById(id)
+    }
+}
