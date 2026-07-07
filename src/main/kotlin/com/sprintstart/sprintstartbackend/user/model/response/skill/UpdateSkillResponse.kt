9d1fd4493d771775f0eb2feb04e6d9af641c2896
BrainSkript <a.johnsen02@gmail.com>
2026-07-07 12:50:40 +0200

    - forgot to push the new files

---
+++ src/main/kotlin/com/sprintstart/sprintstartbackend/user/model/response/skill/UpdateSkillResponse.kt
@@ -0,0 +1,12 @@
+package com.sprintstart.sprintstartbackend.user.model.response.skill
+
+import com.sprintstart.sprintstartbackend.user.external.enums.SkillStatus
+import java.util.UUID
+
+data class UpdateSkillResponse(
+    val id: UUID,
+    val name: String,
+    val roleIds: List<UUID>,
+    val description: String?,
+    val status: SkillStatus,
+)
